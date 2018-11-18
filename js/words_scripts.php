a4de36b03dcd6359eff3e3e68079ce7ac644c651
root <orli-knop@ya.ru>
2018-11-18 19:15:07 +0200

    added filters for word list

--- js/words_scripts.php
+++ js/words_scripts.php
@@ -1,19 +1,49 @@
 <script>
-	$("td[oid], #create_word").on('click', function(){
-		const data = $(this).attr('oid') != undefined ? 'id=' + $(this).attr('oid') : '';
+	$("#part_of_speech_select, #topic_select").change(function() {
+		const topic = $("#topic_select").val();
+		const part_of_speech = $("#part_of_speech_select").val();
+		console.log(topic, part_of_speech);
 		$.ajax({
-				method: 'GET',
-				data: data,
-				dataType: 'text',
-				url: 'ajax/word_form.php',
-				success: function(data) {
-					$(".modal-content").html(data);
-					$(".modal").modal('show');
+			method: 'GET',
+			data: 'topic=' + topic + '&part_of_speech=' + part_of_speech,
+			dataType: 'text',
+			url: '',
+			success: function(data) {
+				$('#list_wrapper').parent().html($(data).find('#list').parent().html());
+				$('table').DataTable({
+					"language": {
+						"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
+					},
+					"scrollX": true,
+					"pageLength": 50
+				} );
+				$("td[oid], #create_word").on('click', function(){
+					edit_word($(this));
+				});
+			},
+			error: function(data) {
+				console.log('error', data);
+			}
+		})
+	});
+	function edit_word(el) {
+			const data = $(el).attr('oid') != undefined ? 'id=' + $(el).attr('oid') : '';
+		$.ajax({
+			method: 'GET',
+			data: data,
+			dataType: 'text',
+			url: 'ajax/word_form.php',
+			success: function(data) {
+				$(".modal-content").html(data);
+				$(".modal").modal('show');
 
-				},
-				error: function(data) {
-					console.log('error', data);
-				}
-			});
+			},
+			error: function(data) {
+				console.log('error', data);
+			}
+		});
+	}
+	$("td[oid], #create_word").on('click', function(){
+		edit_word($(this));
 	});
 </script>
--- module/words.php
+++ module/words.php
@@ -4,15 +4,42 @@
 ?>
 <div class="container mt-5 mb-5">
 	<h1>Слова | מילים</h1>
+	<?php
+		$partofspeech = isset($_GET['part_of_speech']) ? addslashes($_GET['part_of_speech']) : '';
+		$topic = isset($_GET['topic']) ? addslashes($_GET['topic']) : '';
+	?>
 	<div class="row">
+		<div class="col-12 col-md-3 mt-2 mb-2">
+			<select class="form-control" id="part_of_speech_select">
+				<option <?php if($partofspeech == '') echo 'selected';?> value="">Все слова</option>
+				<option <?php if($partofspeech == 'noun') echo 'selected';?> value="noun">Сущ. | שם עצם</option>
+				<option <?php if($partofspeech == 'verb') echo 'selected';?> value="verb">Глаг. | פועל</option>
+				<option <?php if($partofspeech == 'adj') echo 'selected';?> value="adj">Прил. | שם תואר</option>
+				<option <?php if($partofspeech == 'number') echo 'selected';?> value="number">Числ. | מספר</option>
+			</select>
+		</div>
+		<div class="col-12 col-md-3 mt-2 mb-2">
+			<select class="form-control" id = "topic_select">
+				<option  <?php if($topic == '') echo 'selected';?> value="">Любая тема</option>
+				<?php
+					if($list = $conn->query('select name, slug from topic order by name, slug')) {
+						while($row = $list->fetch_assoc()) {
+							?>
+							<option <?php if($topic == $row['slug']) echo 'selected';?> value="<?php echo $row['slug'];?>"><?php echo $row['name'];?></option>
+							<?php
+						}
+					}
+				?>
+			</select>
+		</div>
+		
 		<?php
 			if($user_role == 'admin') {?>
-				<div class="col-12 text-right">
+				<div class="col-md-6 col-12 text-right mt-2 mb-2">
 					<button type="button" id="create_word" class="btn btn-primary">Добавить новое слово<hr>הוסף מילה חדשה</button>
 				</div>
 			<?php }
 		?>
-
 		<div class="col-12 mt-4 mb-2">
 			<table id="list" class="table table-striped table-bordered display nowrap" style="width:100%">
 				<thead>
@@ -24,7 +51,8 @@
 				</thead>
 				<tbody>
 				<?php
-					if($list = $conn->query('select id, word, comment, translation, gender, plural, plural_translation, part_of_speech from word order by word, translation')) {
+					$sql = 'select w.id id, word, comment, translation, gender, plural, plural_translation, part_of_speech from word w left join topic t on t.id = w.topic_id where ("'.$partofspeech.'" = "" or part_of_speech = "'.$partofspeech.'") and ("'.$topic.'" = "" or t.slug = "'.$topic.'") order by word, translation';
+					if($list = $conn->query($sql)) {
 						while($row = $list->fetch_assoc()) {
 							echo '<tr><td>'.$row['translation'].'</td>';
 							echo '<td class="hebrew link" oid="'.$row['id'].'">'.$row['word'].(isset($row['comment']) ? ' '.$row['comment'] : '');
