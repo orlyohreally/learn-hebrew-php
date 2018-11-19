022fc046456f98f9f6ec53076b8eb57608b09d5e
root <orli-knop@ya.ru>
2018-11-19 14:29:18 +0200

    minor fixes

--- module/words.php
+++ module/words.php
@@ -11,7 +11,7 @@
 	<div class="row">
 		<div class="col-12 col-md-3 mt-2 mb-2">
 			<select class="form-control" id="part_of_speech_select">
-				<option <?php if($partofspeech == '') echo 'selected';?> value="">Все слова</option>
+				<option <?php if($partofspeech == '') echo 'selected';?> value="">Все слова | כל המילים</option>
 				<option <?php if($partofspeech == 'noun') echo 'selected';?> value="noun">Сущ. | שם עצם</option>
 				<option <?php if($partofspeech == 'verb') echo 'selected';?> value="verb">Глаг. | פועל</option>
 				<option <?php if($partofspeech == 'adj') echo 'selected';?> value="adj">Прил. | שם תואר</option>
@@ -20,7 +20,7 @@
 		</div>
 		<div class="col-12 col-md-3 mt-2 mb-2">
 			<select class="form-control" id = "topic_select">
-				<option  <?php if($topic == '') echo 'selected';?> value="">Любая тема</option>
+				<option  <?php if($topic == '') echo 'selected';?> value="">Любая тема | כל הנושאים</option>
 				<?php
 					if($list = $conn->query('select name, slug from topic order by name, slug')) {
 						while($row = $list->fetch_assoc()) {
@@ -44,6 +44,7 @@
 			<table id="list" class="table table-striped table-bordered display nowrap" style="width:100%">
 				<thead>
 					<tr>
+						<th>№</th>
 						<th>Перевод | תרגום</th>
 						<th>Слово | מילה</th>
 						<th>Мн.ч. | רבים</th>
@@ -51,10 +52,10 @@
 				</thead>
 				<tbody>
 				<?php
-					$sql = 'select w.id id, word, comment, translation, gender, plural, plural_translation, part_of_speech from word w left join topic t on t.id = w.topic_id where ("'.$partofspeech.'" = "" or part_of_speech = "'.$partofspeech.'") and ("'.$topic.'" = "" or t.slug = "'.$topic.'") order by word, translation';
+					$sql = 'select a.*, (@r := @r+1) seq from ( select w.id id, word, comment, translation, gender, plural, plural_translation, part_of_speech from word w left join topic t on t.id = w.topic_id where ("'.$partofspeech.'" = "" or part_of_speech = "'.$partofspeech.'") and ("'.$topic.'" = "" or t.slug = "'.$topic.'") order by w.id, word, translation ) as a join (select @r:=0) as y';
 					if($list = $conn->query($sql)) {
 						while($row = $list->fetch_assoc()) {
-							echo '<tr><td>'.$row['translation'].'</td>';
+							echo '<tr><td>'.$row['seq'].'</td><td>'.$row['translation'].'</td>';
 							echo '<td class="hebrew link" oid="'.$row['id'].'">'.$row['word'].(isset($row['comment']) ? ' '.$row['comment'] : '');
 							if($row['part_of_speech']=='noun' && $row['gender'] != '') {
 								echo ' ('.($row['gender'] == 'm' ? 'м.р.' : ($row['gender'] == 'f' ? 'ж.р.' : '')).')';
