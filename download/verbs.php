bd5c13c449398e79e3244f832ad8c36b1f2b1b82
root <orli-knop@ya.ru>
2018-11-20 16:29:14 +0200

    styled xlsx file with verbs

--- download/verbs.php
+++ download/verbs.php
@@ -1,36 +1,62 @@
 <?php
-	require '../includes/connect.php';
-	header("Content-Type: text/csv");
-	header("Content-Disposition: attachment; filename=verbs.csv");
+require '../vendor/autoload.php';
+require '../includes/connect.php';
 
-	function outputCSV($data) {
-	  $output = fopen("php://output", "wb");
-	  foreach ($data as $row)
-	    fputcsv($output, $row); // here you can change delimiter/enclosure
-	  fclose($output);
-	}
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-	
+/*$cache_life = 120;
+$file = 'verbs.xlsx';
+if(!file_exists($file) || (time() - filemtime($file) > $cache_life)) {
+	$diff = time() - filemtime($file);*/
+	$spreadsheet = new Spreadsheet();
+	$sheet = $spreadsheet->getActiveSheet();
+
 	if($list = $conn->query('select f.id form_id, v.past_ms, f.name v_group, v.id, infinitive, ms, fs, mp, fp, translation, form from verb v, verb_form f where f.id = v.form_id order by form_id, ms, translation')) {
 		$form_id = 0;
+		$row_counter = 0;
+
 		while($row = $list->fetch_assoc()) {
 			if ($form_id != $row['form_id']) {
-				outputCSV(array(
-				  	array(""),
-			  		array("", $row['v_group'], ""),
-			  		array("תרגום", "עבר", "שם הפועל", "הווה")
-				));
-				outputCSV(
-				  array(array($row['translation'], $row['past_ms'], $row['ms']. ' | '.$row['fs'], $row['infinitive']))
-				);
+				$row_counter = $row_counter + 1;
+				$sheet->setCellValue('A'.$row_counter, $row['v_group']);
+				$sheet->getStyle('A'.$row_counter)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+				//$sheet->getStyle('B'.$row_counter)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_RED);
+				$sheet->getStyle('A'.$row_counter)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+				$sheet->getStyle('A'.$row_counter)->getFill()->getStartColor()->setARGB('2980b9');
+				$sheet->mergeCells('A'.$row_counter.':D'.$row_counter);
+				$row_counter = $row_counter + 1;
+				$sheet->setCellValue('A'.$row_counter, "תרגום");
+				$sheet->setCellValue('B'.$row_counter, "עבר");
+				$sheet->setCellValue('C'.$row_counter, "הווה");
+				$sheet->setCellValue('D'.$row_counter, "שם הפועל");
+				$sheet->getStyle('A'.$row_counter.':D'.$row_counter)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+				$sheet->getStyle('A'.$row_counter.':D'.$row_counter)->getFill()->getStartColor()->setARGB('6ab0de');
+				$row_counter = $row_counter + 1;
+				$sheet->setCellValue('A'.$row_counter, $row['translation']);
+				$sheet->setCellValue('B'.$row_counter, $row['past_ms']);
+				$sheet->setCellValue('C'.$row_counter, $row['ms']. ' | '.$row['fs']);
+				$sheet->setCellValue('D'.$row_counter, $row['infinitive']);
+				
 			}
 			else {
-				outputCSV(array(
-				  array($row['translation'], $row['past_ms'], $row['ms']. ' | '.$row['fs'], $row['infinitive'])
-				));
+				$row_counter = $row_counter + 1;
+				$sheet->setCellValue('A'.$row_counter, $row['translation']);
+				$sheet->getStyle('A'.$row_counter)->getAlignment()->setWrapText(false);
+				$sheet->setCellValue('B'.$row_counter, $row['past_ms']);
+				$sheet->setCellValue('C'.$row_counter, $row['ms']. ' | '.$row['fs']);
+				$sheet->setCellValue('D'.$row_counter, $row['infinitive']);
 			}
 			$form_id = $row['form_id'];
 		}
 	}
-	
+	/*date_default_timezone_set('Asia/Jerusalem');
+	$sheet->setCellValue('A1', $diff.' Файл был обновлен: '.date("F d Y H:i:s.", time()));*/
+	$sheet->getColumnDimension('A')->setAutoSize(true);
+	$writer = new Xlsx($spreadsheet);
+	$writer->save('verbs.xlsx');
+//}
+
+header("Location: verbs.xlsx");
+
 ?>
